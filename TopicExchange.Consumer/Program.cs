72c8a32149c08e0d589477fd7e2e90d9752942df
Krzysiek Wlazlik <krzysiek.wlazlik@gmail.com>
2019-12-19 09:12:52 +0100

    Topic exchange samples

---
+++ TopicExchange.Consumer/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// ReSharper disable ArgumentsStyleNamedExpression
+// ReSharper disable ArgumentsStyleLiteral
+// ReSharper disable ArgumentsStyleStringLiteral
+
+namespace RabbitSamples.TopicExchange.Consumer
+{
+   internal class Program
+   {
+      public static void Main()
+      {
+         ConnectionFactory factory = new ConnectionFactory();
+         using IConnection connection = factory.CreateConnection();
+
+         using IModel channel = connection.CreateModel();
+
+         channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
+         string queueName = channel.QueueDeclare().QueueName;
+
+         string[] bindingKeys =
+         {
+            "*.spicy.pepper",
+            "green.*.tomato",
+            "red.#",
+            "#.carrot"
+         };
+
+         string bindingKey = bindingKeys.Pick();
+
+         channel.QueueBind(queue: queueName,
+            exchange: "topic_logs",
+            routingKey: bindingKey);
+
+         Console.WriteLine($"Waiting for messages matches: {bindingKey}");
+
+         EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+         consumer.Received += (model, ea) =>
+         {
+            byte[] body = ea.Body;
+            string message = Encoding.UTF8.GetString(body);
+            string routingKey = ea.RoutingKey;
+
+            Console.WriteLine($"Message received: '{routingKey}':'{message}'");
+         };
+
+         channel.BasicConsume(queue: queueName,
+            autoAck: true,
+            consumer: consumer);
+
+         Console.ReadLine();
+      }
+   }
+
+   internal static class ListExtensions
+   {
+      private static readonly Random Random = new Random();
+
+      public static T Pick<T>(this IReadOnlyList<T> list)
+      {
+         return list[Random.Next(list.Count)];
+      }
+   }
+}
---
+++ TopicExchange.Producer/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+using RabbitMQ.Client;
+
+// ReSharper disable ArgumentsStyleNamedExpression
+// ReSharper disable ArgumentsStyleLiteral
+// ReSharper disable ArgumentsStyleStringLiteral
+
+namespace RabbitSamples.TopicExchange.Producer
+{
+   internal class Program
+   {
+      public static void Main()
+      {
+         ConnectionFactory factory = new ConnectionFactory();
+
+         using IConnection connection = factory.CreateConnection();
+
+         using IModel channel = connection.CreateModel();
+
+         channel.ExchangeDeclare(exchange: "topic_logs", type: ExchangeType.Topic);
+
+         string[] colors =
+         {
+            "red",
+            "green",
+            "yellow"
+         };
+
+         string[] taste =
+         {
+            "sweet",
+            "spicy"
+         };
+
+         string[] vegetables =
+         {
+            "carrot",
+            "tomato",
+            "pepper"
+         };
+
+         while (true)
+         {
+            string routingKey = $"{colors.Pick()}.{taste.Pick()}.{vegetables.Pick()}";
+            string message = $"New fresh {routingKey.Replace('.', ' ')}";
+            byte[] body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "topic_logs", routingKey: routingKey, basicProperties: null, body: body);
+
+            Console.WriteLine("Message sent: {0}", message);
+
+            Thread.Sleep(1000);
+         }
+      }
+   }
+
+   internal static class ListExtensions
+   {
+      private static readonly Random Random = new Random();
+
+      public static T Pick<T>(this IReadOnlyList<T> list)
+      {
+         return list[Random.Next(list.Count)];
+      }
+   }
+}
